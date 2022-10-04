bf724270c488e92018248d4c4c87e2d26be356c2
Ifiokobong Offiong <i.offiong@reply.de>
2022-10-04 10:16:26 +0200

    new change9

--- infra/main.tf
+++ infra/main.tf
@@ -18,7 +18,7 @@ module "windowsservers" {
   resource_group_name = azurerm_resource_group.example.name
   is_windows_image    = true
   vm_hostname         = "mywinvm" // line can be removed if only one VM module per resource group
-  admin_password      = "ComplxP@ssw0rd!"
+  admin_password      = var.vm_pwd
   vm_os_simple        = "WindowsServer"
   public_ip_dns       = ["winsimplevmips"] // change to a unique name per datacenter region
   vnet_subnet_id      = module.network.vnet_subnets[0]
--- infra/terraform.tfvars
+++ infra/terraform.tfvars
@@ -1,2 +1,2 @@
 # win_os_image = "2019-Datacenter"
-rg_name = "rg-example"
+# rg_name = "rg-example"
